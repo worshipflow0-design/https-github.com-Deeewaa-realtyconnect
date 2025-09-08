71b0ce79ae934f1b7a8da1cb4aea96009e8f9f53
Twinnie Dee <twinniedee20@gmail.com>
2025-09-08 12:52:29 +0000

    now give it a theme that is a clean, uncluttered layout with ample white

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", "sans-serif"],
+        sans: ["Inter", "sans-serif"],
         headline: ['Inter', 'sans-serif'],
         code: ['monospace'],
       },
